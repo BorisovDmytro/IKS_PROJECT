2fb9ee295b7c32cd48fd28e94a46ddf4550bde77
BorisovDmytro <vabidi@yandex.ua>
2016-12-18 19:17:48 +0200

    Add messenger ctrl

--- databaseControllers/DBGroupCtrl.js
+++ databaseControllers/DBGroupCtrl.js
@@ -1,9 +1,41 @@
 "use strict"
 
+/**
+ *  _id
+ *  name
+ *  owner
+ *  users
+ */
+
 class DBGroupCtrl {
   constructor(db) {
     this.collection = db.collection('group');  
   }
+
+  add(name, owner, cb) {
+
+  }
+
+  update(id, obj, cb) {
+
+  }
+
+  removeById(id, cb) {
+
+  }
+
+  removeByName(name, cb) {
+
+  }
+
+  getById(id, cb) {
+
+  }
+
+  getByName(name, cb) {
+    
+  }
+
 }
 
 module.exports = DBGroupCtrl;
