c73412cca6e95d1a7167fe1eba7f9d800d6e4199
VitaiyNikolaenko <ogneslav95@mail.ru>
2016-12-20 18:09:59 +0200

    signUp function was created

--- controllers/AuthCtrl.js
+++ controllers/AuthCtrl.js
@@ -6,35 +6,55 @@ class AuthCtrl {
   constructor(accountCtrl) {
     this.accountCtrl = accountCtrl;
   }
-// {email: "1@gmail.com", pass: "55446"}
+  // {email: "1@gmail.com", pass: "55446"}
 
-// /agaga/:params?data=10
-//agaga/20?data=10 
-//req.params.params // 20
-//req.query.data // 10
-  login(req, res) {  
+  // /agaga/:params?data=10
+  //agaga/20?data=10 
+  //req.params.params // 20
+  //req.query.data // 10
+  login(req, res) {
     const email = req.body.email;
-    const pass  = req.body.pass;
+    const pass = req.body.pass;
 
-    if(!email || !pass) 
+    if (!email || !pass)
       res.status(404).send("not valid data");
     else {
       this.accountCtrl.getByEmail(email, (err, account) => {
-        if(err) 
+        if (err)
           res.status(404).send("not valid data");
         else {
-          if(Encryption.validatePassword(pass, account.pass))
-            res.status(200).send({id: account._id, name: account.name});
-          else 
+          if (Encryption.validatePassword(pass, account.pass))
+            res.status(200).send({ id: account._id, name: account.name });
+          else
             res.status(404).send("not valid data");
-        }  
+        }
       });
-    }  
+    }
   }
 
   signUp(req, res) {
-    // email
-    // pass
+    const email = req.body.email;
+    const pass = req.body.pass;
+    const name = req.body.name;
+
+    if (!email || !pass)
+      res.status(404).send("not valid data");
+    else {
+      dbAccountCtrl.getByEmailOrName(email, name, (err, doc) => {
+        if (!err || doc) {
+          res.status(404).send("not valid data");
+        } else {
+          dbAccountCtrl.insert(email, name, pass, (err, doc) => {
+            if (!err) {
+              console.log("Res:", doc);
+              res.status(200).send("success");
+            } else {
+              res.status(404).send("not valid data");
+            }
+          });
+        }
+      });
+    }
   }
 }
 
