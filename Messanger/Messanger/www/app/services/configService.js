08a2288099b6d7b7d1996ee1e015dbe8561cf513
Darinad Shulyachenko <darina.shulyachenko@gmail.com>
2017-02-05 14:47:03 +0200

    Add auth form with materialize and go to messanger

--- Messanger/Messanger/www/app/services/configService.js
+++ Messanger/Messanger/www/app/services/configService.js
@@ -1,7 +1,11 @@
 ﻿'use strict'
 
 function ConfigService() {
-  this.url = 'http://192.168.0.109:8080/';
+  //this.url = 'http://46.149.86.255:10000/';
+  console.log('Create  config service')
+  this.url = '192.168.0.102';
+  this.port = 10000;
+  this.absUrl = 'http://' + this.url + ':' + this.port;
 }
 
 angular.module('app').service('configService', ConfigService);
