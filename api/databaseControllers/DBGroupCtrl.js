5aef1721f01831599a7ff5ae488ebbe0954752b1
borysov dmytro <vabidi@yandex.ua>
2017-03-08 20:36:59 +0200

    Add promise to database group controller

--- api/databaseControllers/DBGroupCtrl.js
+++ api/databaseControllers/DBGroupCtrl.js
@@ -13,61 +13,104 @@ export default class DBGroupCtrl {
     this.collection = db.collection('group');
   }
 
-  add(name, owner, cb) {
-    this.collection.findOne({ $or: [{ name: name }, { owner: owner }] }, (err, doc) => {
-      if (doc) {
-        cb("data-token");
-      } else {
-        var group = {
+  add(name, owner) {
+    const promise = new Promise((resolve, reject) => {
+      this.collection.findOne({ $or: [{ name: name }, { owner: owner }] }, (err, doc) => {
+        if (doc) {
+          cb("data-token");
+        } else {
+          var group = {
+            name: name,
+            owner: owner,
+            users: []
+          };
+          this.collection.insert(group, { safe: true }, (err, res) => {
+            if (err)
+              reject(err);
+            else
+              resolve(null, res.ops[0]);
+          });
+        }
 
-          name: name,
-          owner: owner,
-          users: []
-        };
-        this.collection.insert(group, { safe: true }, (err, res) => {
-          if (err)
-            cb(err);
-          else
-            cb(null, res.ops[0]);
-        });
-      }
-
+      });
     });
-  }
-  
-  update(id, obj, cb) {
-    this.collection.findOne({ _id: ObjectID(id) }, (err, doc) => {
-      if (err) {
-        cb("not found");
-      } else {
-        doc.name = obj.name || doc.email;
-        doc.owner = obj.owner || doc.owner;
-        doc.users = obj.users || doc.users;
 
-        this.collection.save(doc, { safe: true }, (err) => {
-          if (err)
-            cb("error save");
-          else
-            cb(null, doc);
-        });
-      }
-    });
+    return promise;
   }
 
-  removeById(id, cb) {
-    this.collection.remove({ _id: new ObjectID(id) }, cb);
+  update(id, obj) {
+    const promise = new Promise((res, rej) => {
+      this.collection.findOne({ _id: ObjectID(id) }, (err, doc) => {
+        if (err) {
+          cb("not found");
+        } else {
+          doc.name = obj.name || doc.email;
+          doc.owner = obj.owner || doc.owner;
+          doc.users = obj.users || doc.users;
+
+          this.collection.save(doc, { safe: true }, (err) => {
+            if (err)
+              cb("error save");
+            else
+              cb(null, doc);
+          });
+        }
+      });
+    });
+
+    return promise;
+  }
+
+  removeById(id) {
+    const promise = new Promise((resolve, reject) => {
+      this.collection.remove({ _id: new ObjectID(id) }, (err) => {
+        if (err)
+          reject(err);
+        else
+          resolve();
+      });
+    });
+
+    return promise;
+
   }
 
   removeByName(name, cb) {
-    this.collection.findOne.remove({ name: name }, cb);
+    const promise = new Promise((resolve, reject) => {
+      this.collection.remove({ name: name }, (err) => {
+        if (err)
+          reject(err);
+        else
+          resolve();
+      });
+    });
+
+    return promise;
   }
 
-  getById(id, cb) {
-    this.collection.findOne({ _id: new ObjectID(id) }, cb);
+  getById(id) {
+    const promise = new Promise((res, rej) => {
+      this.collection.findOne({ _id: new ObjectID(id) }, (err, obj) => {
+        if (err)
+          rej(err);
+        else
+          res(obj);
+      });
+    });
+
+    return promise;
   }
 
   getByName(name, cb) {
-    this.collection.findOne({ name: name }, cb);
-  }
+    const promise = new Promise((res, rej) => {
+      this.collection.findOne({ name: name }, (err, obj) => {
+        if (err)
+          rej(err);
+        else
+          res(obj);
+      });
+    });
 
+    return promise;
+  }
 }
